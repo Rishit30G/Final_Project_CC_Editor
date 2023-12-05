a4de662e0f20c80224a5132e91002facc98480e1
Rishit Gupta <74411873+Rishit30G@users.noreply.github.com>
2023-12-05 11:08:10 +0530

    source code added

---
+++ src/components/Editor.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useRef, useState } from 'react';
+import Codemirror from 'codemirror';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/dracula.css';
+import 'codemirror/mode/clike/clike'
+import 'codemirror/addon/edit/closetag';
+import 'codemirror/addon/edit/closebrackets';
+import ACTIONS from '../Actions';
+
+const Editor = ({ socketRef, roomId, onCodeChange }) => {
+    // stpring the editor state 
+    const [text, setText] = useState('');
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+    if (onCodeChange) {
+      onCodeChange(event.target.value);
+    }
+  };
+
+    const editorRef = useRef(null);
+    useEffect(() => {
+        async function init() {
+            editorRef.current = Codemirror.fromTextArea(
+                document.getElementById('realtimeEditor'),
+                {
+                    mode: "text/x-c++src",
+                    theme: 'dracula',
+                    autoCloseTags: true,
+                    autoCloseBrackets: true,
+                    lineNumbers: true,
+                }
+            );
+
+            //editor instance , changes happening event listner attacked on the editor ref whih gives us two things 
+            // its listnenging to 'change', 
+            editorRef.current.on('change', (instance, changes) => {
+                const { origin } = changes;// origin tells us the what thing has happend liek input cut paste 
+                const code = instance.getValue(); // get the whole text we can get it from the instance
+                // we need the parent component to know what the latest code is so for that we send the oncode change as a prop to parent component
+                onCodeChange(code);
+                if (origin !== 'setValue') {
+                    // emmiting the event CODE_CHANGE we sending the data of room_id and the code which we got from instance
+                    socketRef.current.emit(ACTIONS.CODE_CHANGE, {
+                        roomId,
+                        code,
+                    });
+                }
+            });
+        }
+        init();
+    }, []);
+
+    useEffect(() => {
+        if (socketRef.current) {
+            // so we get the code change event from the server and update the code we listning or socket ref
+            socketRef.current.on(ACTIONS.CODE_CHANGE, ({ code }) => {
+                if (code !== null) {
+                    // set value bascialy directly sets what ever you give it to set like useState but for ref
+                    editorRef.current.setValue(code);
+                }
+            });
+        }
+
+        return () => {
+            // swithcing it off
+            socketRef.current.off(ACTIONS.CODE_CHANGE);
+        };
+        // if only we get socket ref
+    }, [socketRef.current]);
+
+    return (
+            <textarea id="realtimeEditor"
+            value={text}
+            onChange={handleTextChange}
+            ></textarea>
+    )
+};
+
+export default Editor;
---
+++ src/pages/EditorPage.js
@@ -0,0 +1,196 @@
+import React, { useState, useRef, useEffect } from 'react';
+import toast from 'react-hot-toast';
+import ACTIONS from '../Actions';
+import Client from '../components/Client';
+import Editor from '../components/Editor';
+import { initSocket } from '../socket';
+import {
+    useLocation,
+    useNavigate,
+    Navigate,
+    useParams,
+} from 'react-router-dom';
+
+const EditorPage = () => {
+    const socketRef = useRef(null);
+    const codeRef = useRef('');
+    const location = useLocation();
+    const { roomId } = useParams();
+    const reactNavigator = useNavigate();
+    const [clients, setClients] = useState([]);
+    const [serverResponse, setServerResponse] = useState(null);
+    const [currentCode, setCurrentCode] = useState(''); 
+    useEffect(() => {
+        codeRef.current = currentCode;
+      }, [currentCode]);
+
+      const handleCodeChange = (newCode) => {
+        setCurrentCode(newCode);
+      };
+      useEffect(() => {
+        // Consider debouncing this call if code changes frequently
+        // sendCodeToServer(); // Uncomment this line if you want to auto-send on change
+      }, [currentCode]);
+
+ 
+
+    useEffect(() => {
+        const init = async () => {
+            socketRef.current = await initSocket();// intializes the socket 
+            socketRef.current.on('connect_error', (err) => handleErrors(err));// looks for if there is a error event 
+            socketRef.current.on('connect_failed', (err) => handleErrors(err));
+            // function to send toast for the error event
+            function handleErrors(e) {
+                // console.log('socket error', e);
+                toast.error('Socket connection failed, try again later.');
+                reactNavigator('/');
+            }
+            // Sending the server that we have joind and sedning the roomId and the user name with it 
+            socketRef.current.emit(ACTIONS.JOIN, {
+                roomId,
+                username: location.state?.username,
+            });
+
+            // Listening for joined event om is for join
+            socketRef.current.on(
+                ACTIONS.JOINED,
+                // we get a call back where we get the data we sent 
+                ({ clients, username, socketId }) => {
+                    // except the currnet user name notify rest of them 
+                    if (username !== location.state?.username) {
+                        toast.success(`${username} joined the room.`);
+                        // console.log(`${username} joined`);
+                    }
+                    // update the clients list 
+                    setClients(clients);
+                    // as soon as the new user joins we send the updated code to the server so that it can give to the new client as we want the new client to get that as soon as it joins
+                    // we also sending the socket id of the new user 
+                    socketRef.current.emit(ACTIONS.SYNC_CODE, {
+                        code: codeRef.current,
+                        socketId,
+                    });
+                }
+            );
+
+            // Listening for disconnected event sent from the server 
+            socketRef.current.on(
+                ACTIONS.DISCONNECTED,
+                // got this as a call back from the server 
+                ({ socketId, username }) => {
+                    // using the user name we can display this 
+                    toast.success(`${username} left the room.`);
+                    // we use filter to update all the clients and set the clients so that the ui gets updated
+                    setClients((prev) => {
+                        return prev.filter(
+                            (client) => client.socketId !== socketId
+                        );
+                    });
+                }
+            );
+        };
+        init();
+        return () => {
+            // on are all the listners so we clear all the listners so that there is no memory leak when the component is unmount
+            socketRef.current.disconnect();
+            // unscbcribing the events
+            socketRef.current.off(ACTIONS.JOINED);
+            socketRef.current.off(ACTIONS.DISCONNECTED);
+        };
+    }, []);
+
+    async function copyRoomId() {
+        try {
+            await navigator.clipboard.writeText(roomId);
+            toast.success('Room ID has been copied to your clipboard');
+        } catch (err) {
+            toast.error('Could not copy the Room ID');
+            console.error(err);
+        }
+    }
+
+    function leaveRoom() {
+        reactNavigator('/');
+    }
+
+    if (!location.state) {
+        return <Navigate to="/" />;
+    }
+
+    const sendCodeToServer = async () => {
+        try {
+          const response = await fetch('http://localhost:10000/compilecode', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ code: currentCode }),
+          });
+          const data = await response.json();
+          setServerResponse(data);
+          console.log(data); // Handle the response data
+        } catch (error) {
+          console.error(error);
+          setServerResponse({ error: 'Failed to fetch data' });
+        }
+    };
+
+    
+    
+
+    return (
+        <div className="mainWrap">
+            <div className="aside">
+                <div className="asideInner">
+                    <div className="logo">
+                        <img
+                            className="logoImage"
+                            src="/Logo.png"
+                            alt="logo"
+                        />
+                    </div>
+                    <h3>Connected</h3>
+                    <div className="clientsList">
+                        {clients.map((client) => (
+                            <Client
+                                key={client.socketId}
+                                username={client.username}
+                            />
+                        ))}
+                    </div>
+                </div>
+                <button onClick={() => sendCodeToServer()} className='btn compileBtn'>
+                    Comiple Code
+                </button>
+                <a target="_blank" href="https://chat-gpt-chatbot.vercel.app/" >
+                    <button className="btn gptBtn"  type="button">
+                    Use ChatGPT
+                </button></a>
+                <button className="btn copyBtn" onClick={copyRoomId} type='button'>
+                    Copy ROOM ID
+                </button>
+                <button className="btn leaveBtn" onClick={leaveRoom} type='button'>
+                    Leave
+                </button>
+                <div className="terminalOutput">
+                 
+                 {serverResponse && (
+                     <pre>{JSON.stringify(serverResponse, null, 2)}</pre>
+                 )}
+                 
+                 {!serverResponse && <p>Terminal Output...</p>}
+                 </div>
+            </div>
+            
+            <div className="editorWrap">
+                <Editor
+                    socketRef={socketRef}
+                    roomId={roomId}
+                    onCodeChange={handleCodeChange}
+                />
+            </div>
+            
+        </div>
+    );
+};
+
+export default EditorPage;
